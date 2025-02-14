0cf6f758a08d6c1ccc6f28704e3fe1f54f7ae7a6
Alan Zhang <alazha@USLKFL-MM6RW57N.local>
2025-02-13 18:38:08 -0800

    feat(button): events

--- src/app/events/events-list.component.ts
+++ src/app/events/events-list.component.ts
@@ -2,12 +2,21 @@ import { Component } from "@angular/core";
 
 @Component({
   selector: 'events-list',
-  templateUrl: './events-list.component.html'
+  template: `
+  <div>
+    <h1> Upcoming Angular Events </h1>
+    <hr />
+    <event-thumbnail 
+    (eventClick)="handleEventClicked($event)"
+    [event]="event1">
+    </event-thumbnail>
+  </div>
+  `
 })
 
 export class EventsListComponent {
-  event = {
-    id : 1,
+  event1 = {
+    id: 1,
     name: 'Angular Connect',
     date: '9/26/2036',
     time: '10:00 am',
@@ -20,4 +29,7 @@ export class EventsListComponent {
     }
   }
 
+  handleEventClicked(data){
+    console.log('received:', data)
+  }
 }
