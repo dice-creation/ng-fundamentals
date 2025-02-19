3d90c1fc70dbdfe2f11ed2e2f7bd6c4c6502354b
Alan Zhang <alazha@USLKFL-MM6RW57N.local>
2025-02-18 22:52:58 -0800

    routing with params

--- src/app/events/event-details/event-details.component.ts
+++ src/app/events/event-details/event-details.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { EventService } from "../shared/event.services";
+import { ActivatedRoute } from "@angular/router";
 
 @Component({
   selector: 'event-details',
@@ -12,10 +13,10 @@ import { EventService } from "../shared/event.services";
 
 export class EventDetailsComponent implements OnInit {
   event: any
-  constructor(private eventService: EventService){
+  constructor(private eventService: EventService, private route:ActivatedRoute){
 
   }
   ngOnInit() {
-    this.event = this.eventService.getEvent(1)
+    this.event = this.eventService.getEvent(+this.route.snapshot.params['id'])
   }
 }
