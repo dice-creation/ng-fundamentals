3c68b5e9feca0cf61b5ed22ee63b18122309c845
Alan Zhang <alazha@USLKFL-MM6RW57N.local>
2025-02-19 16:17:35 -0800

    type safelty IEvent

--- src/app/events/event-details/event-details.component.ts
+++ src/app/events/event-details/event-details.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import { EventService } from "../shared/event.services";
 import { ActivatedRoute } from "@angular/router";
+import { IEvent } from "../shared/index";
 
 @Component({
   selector: 'event-details',
@@ -12,7 +13,7 @@ import { ActivatedRoute } from "@angular/router";
 })
 
 export class EventDetailsComponent implements OnInit {
-  event: any
+  event: IEvent
   constructor(private eventService: EventService, private route:ActivatedRoute){
 
   }
