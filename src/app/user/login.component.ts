e02dcad946ba6b8de0e9c9735661fe2909ca6d99
Alan Zhang <alazha@USLKFL-MM6RW57N.local>
2025-03-04 12:24:02 -0800

    feat: auth service, login and cancel navigation

--- src/app/user/login.component.ts
+++ src/app/user/login.component.ts
@@ -1,4 +1,6 @@
 import { Component } from "@angular/core";
+import { AuthService } from "./auth.service";
+import { Router } from "@angular/router";
 
 @Component ({
   templateUrl: './login.component.html'
@@ -8,7 +10,15 @@ export class LoginComponent {
   userName
   password 
 
+  constructor(private authService: AuthService, private router: Router) {
+  }
+  
   login(formValues) {
-    console.log(formValues)
+    this.authService.loginUser(formValues.userName, formValues.lastName);
+    this.router.navigate(['events'])
+  }
+
+  cancel() {
+    this.router.navigate(['events']);
   }
 }
